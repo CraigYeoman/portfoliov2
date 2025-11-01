0dd472a2a654f1e19acc0693eadd6e0f9bfa5276
Craig Yeoman <yeomancraig@gmail.com>
2025-11-01 13:38:09 -0500

    Initial commit

---
+++ app/components/hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+
+  return (
+    <div>
+      <div>
+        <h1>Software Developer <br />Craig Yeoman</h1>
+        <p>I design solutions that reduce complexity and maximize performance, with a meticulous attention to detail.</p>
+      </div>
+
+
+      <div>
+        <a href="">Get in touch</a>
+        <a href="">My work</a>
+        <a href="">My skills</a>
+      </div>
+    </div>
+  )
+}
