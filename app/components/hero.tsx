15de5592b82ad7b6ae8cd9d9ac0284434316d846
Craig Yeoman <yeomancraig@gmail.com>
2025-11-01 13:58:32 -0500

    Working on skills

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,14 +1,14 @@
 export default function Hero() {
 
   return (
-    <div>
+    <div className="flex">
       <div>
         <h1>Software Developer <br />Craig Yeoman</h1>
         <p>I design solutions that reduce complexity and maximize performance, with a meticulous attention to detail.</p>
       </div>
 
 
-      <div>
+      <div className="flex flex-col">
         <a href="">Get in touch</a>
         <a href="">My work</a>
         <a href="">My skills</a>
---
+++ app/components/skills.tsx
@@ -0,0 +1,34 @@
+export default function Skills() {
+
+  return (
+    <div>
+      <h2>Skills</h2>
+      <div>
+        <h3>Languages</h3>
+        <p>Javascript</p>
+        <p>Typescript</p>
+        <p>C#</p>
+        <p>Go</p>
+      </div>
+      <div>
+        <h2>Front End</h2>
+        <p>React</p>
+        <p>Svelte</p>
+
+      </div>
+      <div>
+        <h2>Backend</h2>
+        <p>Node.js</p>
+        <p>Express</p>
+        <p>.NET</p>
+        <p>Pocketbase</p>
+      </div>
+      <div>
+        <h3>Database</h3>
+        <p>SQLite</p>
+        <p>PostgreSQL</p>
+        <p>MongoDD</p>
+      </div>
+    </div>
+  )
+} 
